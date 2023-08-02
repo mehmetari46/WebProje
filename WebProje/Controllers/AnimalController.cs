fe691a4469fafff6cf0df6fbfcae25e328307ae2
mehmetari46 <mehmetari4641@gmail.com>
2023-08-02 20:52:38 +0300

    önyüz tasarımları ilerleme

---
+++ WebProje/Controllers/AnimalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebProje.Controllers
+{
+    public class AnimalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateDelete()
+        {
+            return View();
+        }
+    }
+}
