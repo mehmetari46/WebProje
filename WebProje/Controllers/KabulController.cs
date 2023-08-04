62df666edc9b86434315ac0d4b80b411be612b31
mehmetari46 <mehmetari4641@gmail.com>
2023-08-04 23:05:39 +0300

    frontend, model, css ilerleme

---
+++ WebProje/Controllers/KabulController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebProje.Controllers
+{
+    public class KabulController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebProje/Models/ModelSahiplen.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebProje.Models
+{
+    public class ModelSahiplen
+    {
+        [Required(ErrorMessage = "Hayvan ID is required")]
+        public int HayvanID { get; set; }
+        [Required(ErrorMessage = "E-Mail is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public int Email { get; set; }
+        public int IsimSoyisim { get; set; }
+        public int TelNo { get; set; }
+        public int Adres { get; set; }
+        [Required(ErrorMessage = "Şifre is required")]
+        public int Sifre { get; set; }
+    }
+}
