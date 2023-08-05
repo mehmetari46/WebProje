220011fa27a2596eb65fa05fc3d153d82ef898a5
mehmetari46 <mehmetari4641@gmail.com>
2023-08-06 00:56:38 +0300

    frontend contreller ilerleme

---
+++ WebProje/Controllers/TakipController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebProje.Controllers
+{
+    public class TakipController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebProje/Models/ModelHayvanKabul.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebProje.Models
+{
+    public class ModelHayvanKabul
+    {
+        [Required]
+        public int HayvanID { get; set; }
+        [Required]
+        public int Animal_Name { get; set; }
+        [Required]
+        public int Animal_Age { get; set; }
+        [Required]
+        public int Animal_Gender { get; set; }
+        [Required]
+        public int Animal_Kind { get; set; }
+    }
+}
